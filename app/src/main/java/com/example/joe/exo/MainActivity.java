de999db4828dbccc7cd4eb2362f750b49a58a207
joe <joe@goqii.com>
2018-05-07 12:42:31 +0530

    handled player state on activity lifecycle

--- app/src/main/java/com/example/joe/exo/MainActivity.java
+++ app/src/main/java/com/example/joe/exo/MainActivity.java
@@ -1,17 +1,14 @@
 package com.example.joe.exo;
 
+import android.content.Intent;
 import android.net.Uri;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.DefaultRenderersFactory;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.drm.DrmSessionManager;
-import com.google.android.exoplayer2.drm.FrameworkMediaCrypto;
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
-import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.dash.DashMediaSource;
 import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
@@ -21,20 +18,65 @@ import com.google.android.exoplayer2.trackselection.TrackSelection;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.upstream.TransferListener;
 import com.google.android.exoplayer2.util.Util;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PlayerView playerView;
+    private SimpleExoPlayer player;
+    private DefaultTrackSelector trackSelector;
+    private com.example.joe.exo.Util util;
+    private int resumeWindow;
+    private long resumePosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        PlayerView playerView = findViewById(R.id.player_view);
+        playerView = findViewById(R.id.player_view);
+        util = new com.example.joe.exo.Util();
+    }
 
-        com.example.joe.exo.Util util = new com.example.joe.exo.Util();
+    @Override
+    public void onNewIntent(Intent intent) {
+        releasePlayer();
+        clearResumePosition();
+        setIntent(intent);
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (Util.SDK_INT <= 23 || player == null) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+
+    private void initializePlayer() {
         DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
 
         DataSource.Factory dataSourceFactory = util.buildDataSourceFactory(this, bandwidthMeter);
@@ -42,24 +84,16 @@ public class MainActivity extends AppCompatActivity {
         TrackSelection.Factory adaptiveTrackSelectionFactory =
                 new AdaptiveTrackSelection.Factory(bandwidthMeter);
 
-        DefaultTrackSelector trackSelector = new DefaultTrackSelector(adaptiveTrackSelectionFactory);
+        trackSelector = new DefaultTrackSelector(adaptiveTrackSelectionFactory);
 //        trackSelectionHelper = new TrackSelectionHelper(trackSelector, adaptiveTrackSelectionFactory);
 
         int extensionRendererMode = 0;
         DefaultRenderersFactory renderersFactory = new DefaultRenderersFactory(this,
                 null, extensionRendererMode);
 
-        SimpleExoPlayer player = ExoPlayerFactory.newSimpleInstance(renderersFactory, trackSelector);
-//        player.addListener(new PlayerEventListener());
-//        player.addListener(eventLogger);
-//        player.addMetadataOutput(eventLogger);
-//        player.addAudioDebugListener(eventLogger);
-//        player.addVideoDebugListener(eventLogger);
-
+        player = ExoPlayerFactory.newSimpleInstance(renderersFactory, trackSelector);
         playerView.setPlayer(player);
-
         player.setPlayWhenReady(true);
-
 
 //        MediaSource mediaSource = new ExtractorMediaSource(Uri.parse("http://vod.leasewebcdn.com/bbb.flv?ri=1024&rs=150&start=0"),
         MediaSource mediaSource = new DashMediaSource.Factory(
@@ -72,5 +106,30 @@ public class MainActivity extends AppCompatActivity {
                         Uri.parse("https://storage.googleapis.com/wvmedia/clear/h264/tears/tears.mpd"));
 
         player.prepare(mediaSource);
+
+        boolean haveResumePosition = resumeWindow != C.INDEX_UNSET;
+        if (haveResumePosition) {
+            player.seekTo(resumeWindow, resumePosition);
+        }
     }
+
+    private void releasePlayer() {
+        if (player != null) {
+            updateResumePosition();
+            player.release();
+            player = null;
+            trackSelector = null;
+        }
+    }
+
+    private void updateResumePosition() {
+        resumeWindow = player.getCurrentWindowIndex();
+        resumePosition = Math.max(0, player.getContentPosition());
+    }
+
+    private void clearResumePosition() {
+        resumeWindow = C.INDEX_UNSET;
+        resumePosition = C.TIME_UNSET;
+    }
+
 }
